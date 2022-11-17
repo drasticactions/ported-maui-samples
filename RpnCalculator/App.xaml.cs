0a25379a3d74be43b802b7d2fbb0bb6540019f37
Timothy Miller <innerlogic4321@gmail.com>
2022-11-17 17:47:10 +0900

    Add RpnCalculator

---
+++ RpnCalculator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RpnCalculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ RpnCalculator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RpnCalculator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
